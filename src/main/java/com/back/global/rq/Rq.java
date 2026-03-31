df0312aad165a5d738a486ee8889e43574d357a3
alison0525 <ak501636@gmail.com>
2026-03-31 11:15:41 +0900

    기존 헤더 방식과 쿠키 방식을 추가

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -10,6 +10,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 @RequiredArgsConstructor
 public class Rq {
@@ -32,16 +34,29 @@ public class Rq {
     public Member getActor() {
 
         String authorizationHeader = request.getHeader("Authorization");
+        String apiKey = null;
 
-        if(authorizationHeader == null){
-            throw new ServiceException("401-1", "인증 정보가 헤더에 존재하지 않습니다.");
+        //헤더 방식
+        if(authorizationHeader != null){
+            if(!authorizationHeader.startsWith("Bearer ")){
+                throw new ServiceException("401-2","잘못된 형식의 인증 데이터입니다.");
+            }
+
+            apiKey = authorizationHeader.replace("Bearer ","");
+
+        }else{
+            //쿠키 방식
+            apiKey = request.getCookies() == null ? ""
+                        : Arrays.stream(request.getCookies())
+                        .filter(cookie -> cookie.getName().equals("apiKey"))
+                        .map(Cookie::getValue)
+                        .findFirst()
+                        .orElse("");
         }
 
-        if(!authorizationHeader.startsWith("Bearer ")){
-            throw new ServiceException("401-2","잘못된 형식의 인증 데이터입니다.");
+        if(apiKey.isBlank()){
+            throw new ServiceException("401-3", "인증 정보가 존재하지 않습니다.");
         }
-
-        String apiKey = authorizationHeader.replace("Bearer ","");
 
 
         return memberService.findByApiKey(apiKey).orElseThrow(
