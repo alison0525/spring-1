078100e6f5b8c3af96f6160c688968576006cd12
alison0525 <ak501636@gmail.com>
2026-04-02 11:35:12 +0900

    스프링 시큐리티 시작, 관리자 추가

---
+++ src/main/java/com/back/domain/member/controller/ApiV1AdmMemberController.java
@@ -0,0 +1,26 @@
+package com.back.domain.member.controller;
+
+
+import com.back.domain.member.dto.MemberWithUsernameDto;
+import com.back.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/adm/members")
+@RequiredArgsConstructor
+public class ApiV1AdmMemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping
+    public List<MemberWithUsernameDto> list(){
+        return memberService.findAll().stream()
+                .map(MemberWithUsernameDto::new)
+                .toList();
+    }
+}
